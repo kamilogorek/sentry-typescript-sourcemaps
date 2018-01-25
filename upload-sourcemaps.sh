367b2195b8c9ddccad195c6afc707bd51fe50805
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-01-25 12:37:47 +0100

    Fully working example

--- upload-sourcemaps.sh
+++ upload-sourcemaps.sh
@@ -2,7 +2,7 @@
 export RELEASE=`date +%s`
 rm -rf ./dist
 ./node_modules/.bin/sentry-cli releases new $RELEASE
-./node_modules/.bin/tsc
+./node_modules/.bin/tsc -p tsconfig.production.json
 ./node_modules/.bin/sentry-cli releases files $RELEASE upload-sourcemaps ./dist --rewrite
 ./node_modules/.bin/sentry-cli releases finalize $RELEASE
 node ./dist/index.js
